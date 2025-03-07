2d222942086e5876d12788c6c592e38a79865d84
Ronnie Gaensli <5719274+gaensli@users.noreply.github.com>
2025-03-07 14:29:47 +0100

    Beat.py add more standard colors

--- src/Beat.py
+++ src/Beat.py
@@ -8,7 +8,10 @@ import Adafruit_GPIO.SPI as SPI
 RED = [255, 0, 0]
 GREEN = [0, 255, 0]
 BLUE = [0, 0, 255]
+CYAN = [0, 255, 255]
 YELLOW = [255, 255, 0]
+MAGENTA = [255, 0, 255]
+GREY = [50, 50, 50]
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
 
