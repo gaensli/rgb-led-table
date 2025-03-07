6c9c84dc61b21ee3276d9c0e89a26dbac21698e6
Ronnie Gaensli <5719274+gaensli@users.noreply.github.com>
2025-03-07 14:29:45 +0100

    Sprache.py: NFC reformat code

--- Sprache.py
+++ Sprache.py
@@ -1,17 +1,20 @@
 import sys
 import pyttsx
- 
+
+
 # main() function
 def main():
-  # use sys.argv if needed
-  print 'running speech-test.py...'
-  engine = pyttsx.init()
-  str = "I speak. Therefore. I am.  "
-  if len(sys.argv) > 1:
-    str = sys.argv[1]
-  engine.say(str)
-  engine.runAndWait()
- 
+    # use sys.argv if needed
+    print
+    'running speech-test.py...'
+    engine = pyttsx.init()
+    str = "I speak. Therefore. I am.  "
+    if len(sys.argv) > 1:
+        str = sys.argv[1]
+    engine.say(str)
+    engine.runAndWait()
+
+
 # call main
 if __name__ == '__main__':
-  main()
+    main()
