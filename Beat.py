409bb967d95d14065c2160de8880f2090d2e751a
Ronnie Gaensli <5719274+gaensli@users.noreply.github.com>
2025-03-07 14:29:45 +0100

    NFC: reformat code

--- Beat.py
+++ Beat.py
@@ -1,6 +1,5 @@
-# Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
+# Simple demo of the WS2801/SPI-like addressable RGB LED lights.
 import time
-import RPi.GPIO as GPIO
 
 # Import the WS2801 module.
 import Adafruit_WS2801
@@ -12,7 +11,7 @@ PIXEL_COUNT = 288
 # Alternatively specify a hardware SPI connection on /dev/spidev0.0:
 SPI_PORT = 0
 SPI_DEVICE = 0
-pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
+pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
 # Define the wheel function to interpolate between different hues.
@@ -31,10 +30,10 @@ def wheel(pos):
 def rainbow_cycle_successive(pixels, wait=0.1):
     for i in range(pixels.count()):
         # tricky math! we use each pixel as a fraction of the full 96-color wheel
-        # (thats the i / strip.numPixels() part)
+        # (that is the i / strip.numPixels() part)
         # Then add in j which makes the colors go around per pixel
         # the % 96 is to make the wheel cycle around
-        pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256))
+        pixels.set_pixel(i, wheel((i * 256 // pixels.count()) % 256))
         pixels.show()
         if wait > 0:
             time.sleep(wait)
@@ -52,7 +51,7 @@ def rainbow_cycle(pixels, wait=0.005):
 def rainbow_colors(pixels, wait=0.05):
     for j in range(256):  # one cycle of all 256 colors in the wheel
         for i in range(pixels.count()):
-            pixels.set_pixel(i, wheel(((256 // pixels.count() + j)) % 256))
+            pixels.set_pixel(i, wheel((256 // pixels.count() + j) % 256))
         pixels.show()
         if wait > 0:
             time.sleep(wait)
@@ -91,13 +90,13 @@ def appear_from_back(pixels, color=(255, 0, 0)):
     for i in range(pixels.count()):
         for j in reversed(range(i, pixels.count())):
             pixels.clear()
-            # first set all pixels at the begin
+            # first set all pixels at the beginning
             for k in range(i):
                 pixels.set_pixel(k, Adafruit_WS2801.RGB_to_color(color[0], color[1], color[2]))
             # set then the pixel at position j
             pixels.set_pixel(j, Adafruit_WS2801.RGB_to_color(color[0], color[1], color[2]))
             pixels.show()
-            #time.sleep(0.001)
+            # time.sleep(0.001)
 
 
 if __name__ == "__main__":
@@ -105,19 +104,18 @@ if __name__ == "__main__":
     pixels.clear()
     pixels.show()  # Make sure to call show() after changing any pixels!
 
-    #rainbow_cycle_successive(pixels, wait=0.1)
+    # rainbow_cycle_successive(pixels, wait=0.1)
     rainbow_cycle(pixels, wait=0.01)
 
     brightness_decrease(pixels)
 
-    #appear_from_back(pixels)
+    # appear_from_back(pixels)
 
-    #for i in range(3):
+    # for i in range(3):
     #    blink_color(pixels, blink_times=1, color=(255, 0, 0))
     #    blink_color(pixels, blink_times=1, color=(0, 255, 0))
     #    blink_color(pixels, blink_times=1, color=(0, 0, 255))
 
-    #rainbow_colors(pixels)
+    # rainbow_colors(pixels)
 
-    #brightness_decrease(pixels)
-print ("hello world")
+    # brightness_decrease(pixels)
